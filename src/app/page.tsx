bbd485c82d57baa53c628b66634cc6f5128c0232
Gabe-Goodman <goodman.g@husky.neu.edu>
2025-01-03 15:55:59 -0500

    Added title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import Canvas from "./components/canvas";
 
 export default function IndexPage() {
-	return <Canvas />;
+	return (
+		<div>
+			<Head>
+				<title>{"Stale Journey (Jaws II)"}</title>
+			</Head>
+			<Canvas />
+		</div>
+	);
 }
